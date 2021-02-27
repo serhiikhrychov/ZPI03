5b3ae8a9cb900fa66bc0a1180365dcb31c8f7131
serhii.khrychov <serhii.khrychov@betlab.com>
2021-02-27 15:18:38 +0200

    second commit

---
+++ calc.js
@@ -0,0 +1,9 @@
+module.exports.Taylor = Taylor;
+
+
+function Taylor(/*in*/iCount) {
+return 10;
+}
+
+
+
---
+++ index.js
@@ -0,0 +1,7 @@
+const Calc = require('./calc');
+
+console.log("Taylor series calculator welcomes you");
+
+let t = Calc.Taylor(1);
+
+console.log(t);
