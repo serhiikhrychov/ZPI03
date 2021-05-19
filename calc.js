c656c6172987e01708bae056d513f7643c0349f7
serhii.khrychov <serhii.khrychov@betlab.com>
2021-05-19 19:27:07 +0300

    members from api

--- calc.js
+++ calc.js
@@ -1,9 +1,21 @@
-module.exports.Taylor = Taylor;
+module.exports.TaylorArcsin = TaylorArcsin;
 
 
-function Taylor(/*in*/iCount) {
-return 10;
+function TaylorArcsin(x,n) {
+    let sum = x, e = 2, o = 1, p = 1;
+    for (let i = 2; i <= n; i++) {
+
+        // The power to which 'x' is raised
+        p += 2;
+
+        sum += (o / e) * (Math.pow(x, p) / p);
+
+        // Numerator value
+        o = o * (o + 2);
+
+        // Denominator value
+        e = e * (e + 2);
+    }
+    return sum.toFixed(10);
+
 }
-
-
-
