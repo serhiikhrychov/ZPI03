347724c874467d527ea1bb723c88d1d47c122a45
serhii.khrychov <serhii.khrychov@betlab.com>
2021-05-20 11:03:49 +0300

    Add A

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ console.log("Taylor series calculator welcomes you, lets find arcsin(x)");
 
 let members = 3;
 
-let result = Calc.TaylorArcsin(0.5, members);
+let result = Calc.TaylorArcsinA(0.5, members);
 console.log(result);
